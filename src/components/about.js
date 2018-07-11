7e5350a8f9529cc4866abb7ec169a22438a109bf
James Wang <jamesnewking@gmail.com>
2018-07-11 15:36:01 -0700

    nav bar on top works

---
+++ src/components/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default props => {
+    return (
+        <div>
+            <h1 className="text-center">About Us</h1>
+            <p>Cookie chupa chups muffin. Croissant cake topping wafer sugar plum. Chocolate topping macaroon sweet roll powder tart biscuit carrot cake.</p>
+            <p>Cookie chupa chups muffin. Croissant cake topping wafer sugar plum. Chocolate topping macaroon sweet roll powder tart biscuit carrot cake.</p>
+            <p>Cookie chupa chups muffin. Croissant cake topping wafer sugar plum. Chocolate topping macaroon sweet roll powder tart biscuit carrot cake.</p>
+            <p>Cookie chupa chups muffin. Croissant cake topping wafer sugar plum. Chocolate topping macaroon sweet roll powder tart biscuit carrot cake.</p>
+        </div>
+    )
+};
---
+++ src/components/secret_list.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default props => {
+    return (
+        <div>
+            <h1 className="text-center">Top Secret List</h1>
+            <ul>
+                <li>Kaylie</li>
+                <li>Ashely</li>
+                <li>James</li>
+                <li>Ellen</li>
+                <li>Allison</li>
+                <li>Jeffrey</li>
+                <li>Andrew</li>
+                <li>Ruby</li>
+            </ul>
+        </div>
+    )
+};
